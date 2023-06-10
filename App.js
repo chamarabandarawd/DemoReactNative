a8245f8eeb03cd120131f0c6ca30068ed5e533fb
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-10 10:45:41 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import { View, Text, TextInput, StyleSheet, SafeAreaView } from 'react-native';
+
+export default function AddPost() {
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Load All Post</Text>
+            </View>
+            <View style={styles.content}>
+
+            </View>
+            <View style={styles.footer}>
+                <Text>Copyright @ iCet 2023</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+
+    header: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'black',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    headerText:{
+        color: 'red',
+        fontSize: 30,
+    },
+    content: {
+        flex: 10,
+        borderWidth: 1,
+        borderColor: 'black',
+
+
+    },
+    footer: {
+        flex: 2,
+        borderWidth: 1,
+        borderColor: 'black',
+        justifyContent:'center',
+        alignItems:'center'
+
+    },
+
+});
